5cb2ffbb9356d119499adfc1e4c696a46ca7c7f0
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-01-10 21:10:54 +0300

    task 06-03

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsArr = ingredients
+.map((el) => {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.textContent = el;
+  ingredientsItem.classList.add('item');
+
+  return `<li>${el}</li>`
+})
+.join(' ');
+
+
+const allIngredients = document.createElement('ul');
+allIngredients.id = "ingredients";
+allIngredients.append(ingredientsArr);
+
+
+
+console.log(allIngredients);
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const ulList = document.querySelector('.gallery');
+
+images.forEach(image => {
+  ulList.insertAdjacentHTML(
+    'beforeEnd', `<li><img src="${image.url}" alt="${image.alt}"></li>`);
+});
+
+console.log(ulList);
+
